602398c05a9d53d0c8cc65486a1dd63205308547
tanahara <s10.vermouth.4869@gmail.com>
2021-11-13 18:16:42 +0900

    AdminUserモデルを作成し, マイグレーションを実行

---
+++ app/models/admin_user.rb
@@ -0,0 +1,6 @@
+class AdminUser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :omniauthable
+  # :registerable, :recoverable
+  devise :database_authenticatable, :rememberable, :validatable, :trackable
+end
